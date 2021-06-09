29b4a66bb244dac2e9b20efe3da8df05bbbbd055
Maks <61799914+maaxnikolaevich@users.noreply.github.com>
2021-06-09 15:25:26 +0500

    1st commit
    Определены классы для игры

---
+++ Wolfs/Transform.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace Wolfs
+{
+    public class Transform
+    {
+        public PointF pos;
+        public Size size;
+
+        public Transform(PointF pos, Size size)
+        {
+            this.pos = pos;
+            this.size = size;
+        }
+
+
+    }
+}
